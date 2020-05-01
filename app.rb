6417a15ad3c046e183de967cdf8c1e64292f9ce1
Mariam Haider <mfh.haider@gmail.com>
2020-05-01 16:23:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do 
+    enable :sessions
+    set :sessions_secret, "secret"
+  end 
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  
 end
