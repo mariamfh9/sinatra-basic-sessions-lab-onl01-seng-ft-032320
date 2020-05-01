11300b1573fbcf09bbb7eb1a7ce6767c9eec2d94
Mariam Haider <mfh.haider@gmail.com>
2020-05-01 16:29:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,22 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do 
     enable :sessions
-    set :sessions_secret, "secret"
+    set :sessions_secret, "MySession"
   end 
   
   get '/' do 
     erb :index
   end 
   
+  get '/checkout' do
+      erb :shows
+   end
+
+   post '/checkout' do
+      session[:item] = params[:item]
+      @session = session
+   redirect to "/checkout"
+   end
+end
   
 end
